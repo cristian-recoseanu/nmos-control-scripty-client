e9c380c113fcb738d0fc3945ca17e2917d4731fd
Cristian Recoseanu <cristian.recoseanu@hotmail.com>
2025-10-10 20:03:54 +0100

    Add PR Checks workflow

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+const typescriptEslint = require("@typescript-eslint/eslint-plugin");
+const typescriptParser = require("@typescript-eslint/parser");
+
+module.exports = [
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: "module",
+        project: "./tsconfig.json"
+      },
+      globals: {
+        console: "readonly",
+        process: "readonly",
+        Buffer: "readonly",
+        __dirname: "readonly",
+        __filename: "readonly",
+        module: "readonly",
+        require: "readonly"
+      }
+    },
+    plugins: {
+      "@typescript-eslint": typescriptEslint
+    },
+    rules: {
+      ...typescriptEslint.configs.recommended.rules,
+      // Add your custom rules here
+    }
+  },
+  {
+    ignores: ["node_modules", "dist", "build", "coverage", "**/*.js"]
+  }
+];
